9783cc6e7c4ab026179abf67c08d0fe56fa83713
gsprasannabalaji <govindarajalusaras.p@northeastern.edu>
2024-03-28 01:14:51 -0400

    updated the component for job listing

--- src/pages/JobListing.jsx
+++ src/pages/JobListing.jsx
@@ -1,26 +1,59 @@
-import Paper from '@mui/material/Paper';
-import jobPosts from '../mockData/jobPosts';
+import jobPosts from "../mockData/jobPosts";
 import Box from "@mui/material/Box";
-import { Link, Typography } from '@mui/material';
-import { useTheme } from '@emotion/react';
+import { Card, CardContent, Link, Typography } from "@mui/material";
+import { useTheme } from "@emotion/react";
 
 const JobListing = () => {
-    const theme = useTheme();
+  const theme = useTheme();
 
-    return (
-        <>
-            {jobPosts?.map((jobPost) => (
-                <Paper key={jobPost?.id} sx={{ p: 2, mt: 3, display: 'flex', flexDirection: 'column' }} elevation={5}>
-                    <Typography variant="h5" sx={{ mt:0, mb: 2}} >{jobPost?.title}</Typography>
-                    <Box component="p" sx={{ mt:0, mb: 2}} >{jobPost?.description}</Box>
-                    <Box componen="div" sx={{ display: {xs: 'flex'}, justifyContent: 'space-between', alignItems: {xs: 'baseline', md: 'center'} , flexDirection: {xs: 'column', md: 'row'} }}>
-                    <Link component={'a'} underline='hover' href={jobPost?.applyLink} target='_blank' sx={{ mb: {xs: 1} }}>Apply</Link>
-                    <Box component="p" sx={{ m: 0, color: theme.palette.secondary.main}} >{jobPost?.lastUpdated}</Box>
-                    </Box>
-                </Paper>
-            ))}
-        </>
-    )
-}
+  return (
+    <>
+      {jobPosts?.map((jobPost) => (
+        <Card
+          key={jobPost?.id}
+          sx={{
+            mt: 3,
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <CardContent>
+            <Typography variant="h5" sx={{ mt: 0, mb: 2 }}>
+              {jobPost?.title}
+            </Typography>
+            <Box component="p" sx={{ mt: 0, mb: 2 }}>
+              {jobPost?.description}
+            </Box>
+            <Box
+              componen="div"
+              sx={{
+                display: { xs: "flex" },
+                justifyContent: "space-between",
+                alignItems: { xs: "baseline", md: "center" },
+                flexDirection: { xs: "column", md: "row" },
+              }}
+            >
+              <Link
+                component={"a"}
+                underline="hover"
+                href={jobPost?.applyLink}
+                target="_blank"
+                sx={{ mb: { xs: 1 } }}
+              >
+                Apply
+              </Link>
+              <Box
+                component="p"
+                sx={{ m: 0, color: theme.palette.secondary.main }}
+              >
+                {jobPost?.lastUpdated}
+              </Box>
+            </Box>
+          </CardContent>
+        </Card>
+      ))}
+    </>
+  );
+};
 
 export default JobListing;
