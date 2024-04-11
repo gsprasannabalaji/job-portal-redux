961c5b885e6fe251de29e65e88076b711559e393
gsprasannabalaji <govindarajalusaras.p@northeastern.edu>
2024-04-11 12:04:49 -0400

    updated the login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import Button from "@mui/material/Button";
+import { Button, Grid } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
@@ -7,9 +7,10 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import CustomSnackBar from "../components/CustomSnackBar";
 import { setApiError, setFormData } from "../features/login/loginSlice";
+import loginImage from "../assets/login.webp";
 
 const Login = () => {
-  const { formData, apiError} = useSelector((state) => state?.login);
+  const { formData, apiError } = useSelector((state) => state?.login);
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
@@ -26,7 +27,7 @@ const Login = () => {
         {
           email: formData?.email,
           password: formData?.password,
-        }, 
+        },
         { withCredentials: true }
       );
 
@@ -34,69 +35,95 @@ const Login = () => {
         localStorage.setItem("isAuthenticated", true);
         localStorage.setItem(
           "userDetails",
-          JSON.stringify({ email: formData?.email, fullName: response?.data?.fullName, role: response?.data?.role })
+          JSON.stringify({
+            email: formData?.email,
+            fullName: response?.data?.fullName,
+            role: response?.data?.role,
+          })
         );
-        if(response?.data?.role === "admin") {
+        if (response?.data?.role === "admin") {
           navigate("/employees");
         } else {
           navigate("/home");
         }
       }
     } catch (error) {
-      const errorMessage = error?.response?.data?.message || "Server is down. Please try again later.";
+      const errorMessage =
+        error?.response?.data?.message ||
+        "Server is down. Please try again later.";
       dispatch(setApiError(errorMessage));
     }
   };
 
   return (
     <>
-      <Box
-        sx={{
-          height: "100vh",
-          display: "flex",
-          justifyContent: "center",
-          flexDirection: "column",
-        }}
-      >
-        <Box component="h1" textAlign="center" sx={{ mt: 0 }}>
-          Login
-        </Box>
-        <Box
-          component="form"
-          sx={{
-            "& .MuiTextField-root": { m: 1, width: "50ch" },
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6} md={6}>
+          <img
+            src={loginImage}
+            alt="login"
+            style={{ width: "100%", height: "100vh" }}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} md={6}>
+          <Box
+            sx={{
+              height: "100vh",
+              display: "flex",
+              justifyContent: "center",
+              flexDirection: "column",
+            }}
+          >
+            <Box component="h1" textAlign="center" sx={{ mt: 0 }}>
+              Login
+            </Box>
+            <Box
+              component="form"
+              sx={{
+                "& .MuiTextField-root": { m: 1, width: "90%" },
+              }}
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              flexDirection="column"
+              noValidate
+              autoComplete="off"
+              onSubmit={handleSubmit}
+            >
+              <TextField
+                required
+                id="email"
+                name="email"
+                label="Email Address"
+                onChange={handleChange}
+                sx={{ width: 800 }}
+              />
+              <TextField
+                required
+                type="password"
+                id="password"
+                name="password"
+                label="Password"
+                onChange={handleChange}
+                sx={{ width: 300 }}
+              />
+              <Button type="submit" variant="contained" endIcon={<SendIcon />}>
+                Login
+              </Button>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+      {apiError && (
+        <CustomSnackBar
+          isOpen={true}
+          message={apiError}
+          onClose={() => {
+            dispatch(setApiError(null));
           }}
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          flexDirection="column"
-          noValidate
-          autoComplete="off"
-          onSubmit={handleSubmit}
-        >
-          <TextField
-            required
-            id="email"
-            name="email"
-            label="Email Address"
-            onChange={handleChange}
-            sx={{ width: 800 }}
-          />
-          <TextField
-            required
-            type="password"
-            id="password"
-            name="password"
-            label="Password"
-            onChange={handleChange}
-            sx={{ width: 300 }}
-          />
-          <Button type="submit" variant="contained" endIcon={<SendIcon />}>
-            Login
-          </Button>
-        </Box>
-      </Box>
-      {apiError && <CustomSnackBar isOpen={true} message={apiError} onClose={() => {dispatch(setApiError(null))}} customKey={"login"} />}
+          customKey={"login"}
+        />
+      )}
     </>
   );
 };
